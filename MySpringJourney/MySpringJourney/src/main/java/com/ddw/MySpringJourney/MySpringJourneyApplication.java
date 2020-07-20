3299513e41a6ead87ce8dff7543151c6548390ea
d00890 <dongdawei@smyfinancial.com>
2020-07-20 15:48:07 +0800

    created my spring project

---
+++ MySpringJourney/MySpringJourney/src/main/java/com/ddw/MySpringJourney/MySpringJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.ddw.MySpringJourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringJourneyApplication.class, args);
+	}
+
+}
